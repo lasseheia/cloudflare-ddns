a433047870ddebef7628aa8846b911b319ce666b
lheia <lasse.heia@basefarm.com>
2021-01-03 17:58:18 +0100

    Now getting current IP address.

--- ddns.py
+++ ddns.py
@@ -1,15 +1,23 @@
 from dotenv import load_dotenv, find_dotenv
 import os
+import requests
 import CloudFlare
 
 def main():
+    # Load environment variables from .env file
     load_dotenv(find_dotenv())
     
+    # Get current IP address
+    r = requests.get('https://api.ipify.org?format=json')
+    ip = r.json()['ip']
+
+    # Connect to Cloudflare
     cf = CloudFlare.CloudFlare(email=os.getenv('email'), token=os.getenv('token'))
     zones = cf.zones.get()
     for zone in zones:
         zone_id = zone['id']
         zone_name = zone['name']
         print(zone_id, zone_name)
+
 if __name__ == '__main__':
     main() 
